beca38d9467f727d5d37e4102f4b0d73a58423cb
autumn3195 <autumn3195@gmail.com>
2025-11-20 21:10:13 +0900

    Add files via upload

--- sample3/sketch.js
+++ sample3/sketch.js
@@ -19,90 +19,90 @@ function draw() {
   background(bgColor);
   
   //얼굴형
-  noStroke();
+  noStroke()
   fill(240, 200, 180);
   ellipse(300, 180, 185, 220);
   
   //귀
-  noStroke();
+  noStroke()
   fill(240, 200, 180);
   arc(220, 180, 50, 60, radians(90), radians(315));
   
-  noStroke();
+  noStroke()
   fill(240, 200, 180);
   arc(380, 180, 50, 60, radians(225), radians(90));
   
   //머리카락
-  noStroke();
+  noStroke()
   fill(50, 0, 0);
   arc(275, 100, 160, 150, radians(135), radians(330));
   
-  noStroke();
+  noStroke()
   fill(50, 0, 0);
   arc(325, 110, 160, 150, radians(225), radians(30));
   
-  noStroke();
+  noStroke()
   fill(50, 0, 0);
   triangle(215, 150, 275, 150, 310, 70);
   
-  noStroke();
+  noStroke()
   fill(50, 0, 0);
   triangle(392.5, 145, 325, 150, 290, 70);
   
   //눈
-  noStroke();
+  noStroke()
   fill(255);
   ellipse(260, 170, 50, 25);
   
-  noStroke();
+  noStroke()
   fill(255);
   ellipse(340, 170, 50, 25);
   
   //눈동자
-  noStroke();
+  noStroke()
   fill(50, 0, 0);
   ellipse(lefteye_x, lefteye_y, 17.5, 22.5);
   
-  noStroke();
+  noStroke()
   fill(50, 0, 0);
   ellipse(righteye_x, righteye_y, 17.5, 22.5);
   
   //코
-  stroke(0);
+  stroke(0)
   strokeWeight(2);
   line(300, 180, 285, 215);
   
-  stroke(0);
+  stroke(0)
   strokeWeight(2);
   line(285, 215, 300, 215);
   
   //입
-  stroke(0);
+  stroke(0)
   strokeWeight(2);
   noFill();
   arc(300, 205, 100, 100, radians(30), radians(150));
   
   //목
-  noStroke();
+  noStroke()
   fill(240, 200, 180);
   rect(270, 260, 60, 50);
   
   //옷1
-  noStroke();
+  noStroke()
   fill(255);
   rect(270, 310, 60, 100);
   
   //목걸이
-  stroke(127);
+  stroke(127)
   strokeWeight(2);
   arc(300, 312.5, 62.5, 50, radians(0), radians(180));
   
   //옷2
-  noStroke();
+  noStroke()
   fill(clothedColor);
   quad(130, 400, 200, 325, 275, 300, 300, 400);
   
-  noStroke();
+  noStroke()
   fill(clothedColor);
   quad(480, 400, 400, 325, 325, 300, 300, 400);
 }
@@ -114,18 +114,18 @@ function mousePressed() {
 }
 
 function keyPressed() {
-  if (keyCode === LEFT_ARROW) {
+  if (keyCode === LEFT_ARROW && lefteye_x > 240) {
     lefteye_x -= 10; // 왼쪽 화살표 키를 누르면 왼쪽으로 이동
     righteye_x -= 10;
-  } else if (keyCode === RIGHT_ARROW) {
+  } else if (keyCode === RIGHT_ARROW && righteye_x < 360) {
     lefteye_x += 10; // 오른쪽 화살표 키를 누르면 오른쪽으로 이동
     righteye_x += 10;
-  } else if (keyCode === UP_ARROW) {
-    lefteye_y -= 10; // 위쪽 화살표 키를 누르면 위로 이동
-    righteye_y -= 10;
-  } else if (keyCode === DOWN_ARROW) {
-    lefteye_y += 10; // 아래쪽 화살표 키를 누르면 아래로 이동
-    righteye_y += 10;
+  } else if (keyCode === UP_ARROW && lefteye_y > 165 && righteye_y > 165) {
+    lefteye_y -= 5; // 위쪽 화살표 키를 누르면 위로 이동
+    righteye_y -= 5;
+  } else if (keyCode === DOWN_ARROW && lefteye_y < 175 && righteye_y < 175) {
+    lefteye_y += 5; // 아래쪽 화살표 키를 누르면 아래로 이동
+    righteye_y += 5;
   } else if (key === 's') {
     saveGif('mySketch', 10);
   }
